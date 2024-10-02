a3a20f56c3f4a50c31fa6234d2cead9316f506f7
PoF <54464488+p-o-f@users.noreply.github.com>
2024-10-01 22:50:39 -0700

    Feat/metrics (#3)
    * heart and sleep data running in sim
    
    * heart and sleep data collecting on sim
    
    * minor change for ngrok url

--- app/app-service/service.js
+++ app/app-service/service.js
@@ -1,5 +1,9 @@
-import { Time } from '@zos/sensor'
+import * as notificationMgr from "@zos/notification";
+import { Time } from '@zos/sensor';
 import { BasePage } from "@zeppos/zml/base-page";
+import { HeartRate, Sleep } from "@zos/sensor";
+import { getProfile } from '@zos/user';
+import { getDeviceInfo } from '@zos/device';
 
 const timeSensor = new Time();
 const url = 'insert_ngrok_url_here/post'; // replace with your ngrok tunnel url
@@ -7,33 +11,53 @@ const url = 'insert_ngrok_url_here/post'; // replace with your ngrok tunnel url
 AppService(
     BasePage({
         onInit() {
-            this.log('app service onInit')
+            this.log('app service onInit');
 
             timeSensor.onPerMinute(() => {
                 this.log("app service running");
-                this.httpRequest({
-                    method: 'POST',
-                    url: url,
-                    body: {
-                        "data": "hello from watch"
-                    },
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                }).then(result => {
-                    return result.json();
-                }).then(data => {
-                    this.log(data);
-                }).catch(error => {
-                    this.log(error);
-                });
+                this.sendMetrics(); 
+            });
+        },
+        sendMetrics() {
+            const startTime = new Date().getTime();
+            const deviceInfo = getDeviceInfo();
+            const heartRate = new HeartRate();
+            const sleep = new Sleep();
 
+            sleep.updateInfo();
+
+            const reqBody = {
+                recordTime: Math.floor(new Date().getTime() / 1000),
+                user: getProfile(),
+                device: deviceInfo,
+                heartRateLast: heartRate.getLast(),
+                heartRateResting: heartRate.getResting(),
+                heartRateSummary: heartRate.getDailySummary(),
+                sleepInfo: sleep.getInfo(),
+                sleepStageList: sleep.getStageConstantObj(),
+                sleepStatus: sleep.getSleepingStatus(),
+            };
+
+            this.httpRequest({
+                method: 'POST',
+                url: url,
+                body: JSON.stringify(reqBody),
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            }).then(result => {
+                return result.json();
+            }).then(data => {
+                this.log(data);
+            }).catch(error => {
+                this.log(error);
             });
         },
         onRun() {
-            this.log('app side service onRun')
+            this.log('app side service onRun');
         },
         onDestroy() {
-            this.log('app side service onDestroy')
+            this.log('app side service onDestroy');
         },
-    }));
+    })
+);
