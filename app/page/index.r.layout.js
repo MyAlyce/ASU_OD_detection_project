9cfb44c0c1278a9c7ae22307af26486cef5117a3
Emily Barkley <149705329+EsBarkley@users.noreply.github.com>
2024-11-19 23:37:43 -0700

    Update index.r.layout.js
    Added the additional permission button

--- app/page/index.r.layout.js
+++ app/page/index.r.layout.js
@@ -3,7 +3,7 @@ import { BUTTON_TEXT, BUTTON_TEXT_STOP, NORMAL_COLOR, PRESS_COLOR } from "../uti
 
 export const START_BUTTON = {
   x: px(60),
-  y: px(200),
+  y: px(300),
   w: px(360),
   h: px(80),
   text_size: px(36),
@@ -12,6 +12,7 @@ export const START_BUTTON = {
   press_color: PRESS_COLOR,
   text: BUTTON_TEXT
 };
+
 
 export const STOP_BUTTON = {
   x: px(60),
@@ -23,4 +24,28 @@ export const STOP_BUTTON = {
   normal_color: NORMAL_COLOR,
   press_color: PRESS_COLOR,
   text: BUTTON_TEXT_STOP
+};
+
+export const SLEEP_BUTTON = {
+  x: px(50),
+  y: px(225),
+  w: px(200),
+  h: px(50),
+  text_size: px(25),
+  radius: px(12),
+  normal_color: 0xaa42ee,
+  press_color: 0xb964f0,
+  text: "Sleep Data"
+};
+
+export const PERMISSIONS_BUTTON = {
+  x: px(75),
+  y: px(100),
+  w: px(225),
+  h: px(50),
+  text_size: px(25),
+  radius: px(12),
+  normal_color: 0x0352fc,
+  press_color: 0x487ff7,
+  text: "Permission Data"
 };
