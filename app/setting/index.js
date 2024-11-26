fb8bebb24f20a729b3d82485b2096166f1c3c4e6
ybriker <59706277+yaniv04@users.noreply.github.com>
2024-11-26 00:39:39 -0700

    accidentally removed

--- app/setting/index.js
+++ app/setting/index.js
@@ -113,3 +113,34 @@ AppSettingsPage({
 		return now >= expiresAt;
 	},
 });
+
+/**
+ * Request Google Auth Data from Google API after receiving auth code
+ * @param authResponse the auth code from Google API
+ * @returns access token and other data for using API
+ */
+const requestGoogleAuthData = async (authResponse) => {
+	const params = {
+		grant_type: 'authorization_code',
+		client_id: GOOGLE_API_CLIENT_ID,
+		client_secret: GOOGLE_API_CLIENT_SECRET,
+		redirect_uri: GOOGLE_API_REDIRECT_URI,
+		code: authResponse.code,
+	};
+
+	const body = Object.keys(params)
+		.map(
+			(key) => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`,
+		)
+		.join('&');
+
+	// console.log(body)
+	const data = await fetch('https://oauth2.googleapis.com/token', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+		},
+		body: body,
+	});
+	return await data.json();
+};
