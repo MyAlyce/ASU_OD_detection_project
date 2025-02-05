dcda73b55e7b8c6426e271038de019f9d601a9fa
parth <parthib.faruk@gmail.com>
2025-02-05 01:59:31 -0700

    ignore this commit (placeholder for progress for a future change)

--- app/app-service/service.js
+++ app/app-service/service.js
@@ -5,12 +5,17 @@ import { HeartRate, Sleep } from '@zos/sensor';
 import { getProfile } from '@zos/user';
 import { getDeviceInfo } from '@zos/device';
 import { GoogleApi } from './google-api';
+import { Sequence } from '@silver-zepp/sequence';
 
 const timeSensor = new Time();
 const storage = getApp().globals.storage;
 const tsdb = getApp().globals.tsdb;
 const debug = true;
 const SEND_INTERVAL = 1; // in minutes
+
+
+//TODO address another issue mentioned by Silver: "Another small issue is here. QuickJS doesn't have .error method so your notifyWatch will never execute. Just use .log for everything"
+
 
 AppService(
 	BasePage({
@@ -21,32 +26,39 @@ AppService(
 			const googleApi = new GoogleApi(this, token, refreshToken, expiryDate, null); //the "null" is the sheetId instance variable of google-api.js, which is set at the time of sheet creation
 
 			notifyWatch(`Starting service, token is here? ${!!token}`);
-
-			// Check if the Google Drive folder exists, if not, create it
-			if (!storage.getKey('zeppGoogleFolderId')) { 
-				googleApi.createNewGoogleDriveFolder('test') // TODO change "test" to the user's Google account name, maybe like "John Doe - Zepp Data"
-				.then((response) => {
-					console.log('New folder created:', response);
-
-					const zeppGoogleFolderId = response.id; 
-					storage.setKey('zeppGoogleFolderId', zeppGoogleFolderId);
 			
-					notifyWatch(`Created new Google Drive folde: ${response.name} (ID: ${response.id})`); 
+			let currentSheetId = null;
 
-					//IMPORTANT! ! ! ! ! ! 
-					//TODO add some code that basically clicks the STOP button and then START button here, need to stop this from running and then start again for some reason if a new folder is created?
-					// i think this is probably because in this version (not up to date with main branch) the refresh token function isn't implemented in google-api.js?
-					// also append headers doesn't work, could be related? not sure
-				})
-				.catch((error) => {
-					console.error('Failed to create new Google Drive folder:', error);
-					notifyWatch(`Failed to create new Google Drive folder: ${error.message}`);
-				});
+			//function A
+
+			function checkOrCreateFolder(folderName = "test") { // Check if the Google Drive folder exists, if not, create it
+				if (!storage.getKey('zeppGoogleFolderId')) { 
+					googleApi.createNewGoogleDriveFolder(folderName) // TODO change "test" to the user's Google account name, maybe like "John Doe - Zepp Data"
+					.then((response) => {
+						console.log('New folder created:', response);
+
+						const zeppGoogleFolderId = response.id; 
+						storage.setKey('zeppGoogleFolderId', zeppGoogleFolderId);
+				
+						notifyWatch(`Created new Google Drive folde: ${response.name} (ID: ${response.id})`); 
+
+					})
+					.catch((error) => {
+						console.error('Failed to create new Google Drive folder:', error);
+						notifyWatch(`Failed to create new Google Drive folder: ${error.message}`);
+					});
+				}
 			}
-			
-			let currentSheetId = storage.getKey('currentSheetId') || null;
-			// If there's no sheet at the time when onInit is invoked, create a new one for the current day (so we don't have to wait for onPerDay() to happen)
-			if (!currentSheetId) {
+
+
+
+
+			//function B
+			//TODO 2/5/2025 1:54 AM (left off) in this method removed a check for: if (!currentSheetId) { // If there's no sheet at the time when onInit is invoked, create a new one for the current day (so we don't have to wait for onPerDay() to happen)
+			// do this check manually later, when i call createNewSheet() function
+
+			//use functions tom odel off what was previously done (in the PR basically) and then implement silver's async thingy
+			function createNewSheet() {
 				const today = new Date();
 				const dateTitle = today.toISOString().split('T')[0]; // e.g., "2025-01-24"
 
@@ -58,22 +70,60 @@ AppService(
 				googleApi
 					.createNewGoogleSheet(`zepp ${dateTitle}`, folderId) // in google-api.js, this will use setter method to set the sheetId instance variable before .then() happens
 					.then((response) => {
-						this.log('New spreadsheet created with onInit:', response);
-						this.log('Created spreadhseet in folder with onInit:', folderId);
+						this.log('New spreadsheet created inside of onInit:', response);
+						this.log('Created spreadhseet in folder inside of onInit:', folderId);
 
 						const spreadsheetId = response.spreadsheetId; // get id of the new sheet
 						storage.setKey('currentSheetId', spreadsheetId); // save the id to storage
 
-						notifyWatch(`Created new Google Sheet with onInit: ${response.spreadsheetUrl} in folder ${folderId}`); //TODO this just prints the folderID which is like a url id, printing the actual folder name needs another API call I think
+						notifyWatch(`Created new Google Sheet inside of onInit: ${response.spreadsheetUrl} in folder ${folderId}`); //TODO this just prints the folderID which is like a url id, printing the actual folder name needs another API call I think
 				})
 				.catch((error) => {
-					this.log('Failed to create a new Google Sheet in onInit', error.message);
+					this.log('Failed to create a new Google Sheet inside of onInit', error.message);
 					notifyWatch(
-						`Failed to create a new Google Sheet in onInit: ${JSON.stringify(error.message)}`,
+						`Failed to create a new Google Sheet inside of onInit: ${JSON.stringify(error.message)}`,
 					);
 				});
+				
 			}
-		
+
+
+
+
+			//function C
+
+			function sendDataToGoogle() {
+				const fiveMinutesAgo = Date.now() - 6 * 60 * 1000;
+				const now = Date.now();
+				notifyWatch(`data ${tsdb.retrieveDataSeries(fiveMinutesAgo, now)}`);
+
+				const data = [this.getMetrics()];
+
+				// todo: connectStatus() to check if the phone is connected
+				googleApi
+					.sendDataToGoogleSheets(data)
+					.then((res) => {
+						this.log('Successfully wrote to Google Sheets', res.message);
+						notifyWatch(res.message);
+						// tsdb.purge(fiveMinutesAgo);
+					})
+					.catch((error) => {
+						this.log('Failed to write to Google Sheets', error.message);
+						notifyWatch(
+							`Failed to write to Google Sheets: ${JSON.stringify(error.message)}`,
+						);
+						// TODO save to tsdb for retry later
+					});
+			}
+
+
+			
+			checkOrCreateFolder(); // Call function A
+
+
+
+			
+
 
 			timeSensor.onPerMinute(() => {
 				this.log(
@@ -83,27 +133,7 @@ AppService(
 				// Every minute, save metrics to TSDB
 				// saveToTSDB(this.getMetrics());
 				if (timeSensor.getMinutes() % SEND_INTERVAL == 0) {
-					const fiveMinutesAgo = Date.now() - 6 * 60 * 1000;
-					const now = Date.now();
-					notifyWatch(`data ${tsdb.retrieveDataSeries(fiveMinutesAgo, now)}`);
-
-					const data = [this.getMetrics()];
-
-					// todo: connectStatus() to check if the phone is connected
-					googleApi
-						.sendDataToGoogleSheets(data)
-						.then((res) => {
-							this.log('Successfully wrote to Google Sheets', res.message);
-							notifyWatch(res.message);
-							// tsdb.purge(fiveMinutesAgo);
-						})
-						.catch((error) => {
-							this.log('Failed to write to Google Sheets', error.message);
-							notifyWatch(
-								`Failed to write to Google Sheets: ${JSON.stringify(error.message)}`,
-							);
-							// TODO save to tsdb for retry later
-						});
+					sendDataToGoogle();
 				}
 			});
 
