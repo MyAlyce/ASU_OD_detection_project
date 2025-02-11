c77db9eb08928420e2996c86837efce86dd1d75b
Emil <esbarkle@asu.edu>
2025-02-11 02:12:16 -0700

    Adds a test notification that appears after clicking a button

--- app/page/rescuePlan.js
+++ app/page/rescuePlan.js
@@ -3,22 +3,49 @@ import * as appService from '@zos/app-service';
 import { queryPermission, requestPermission } from '@zos/app';
 import hmUI from '@zos/ui';
 import { push } from '@zos/router';
-import { Sleep } from '@zos/sensor'; // Import the Sleep module
-import { notify } from '@zos/notification'
+import { getText } from "@zos/i18n";
+import * as notificationMgr from "@zos/notification";
 import {
-	TEXT_WIDGET,
+    TEXT_WIDGET,
+    TEST_NOTIFICATION_BUTTON,
 } from 'zosLoader:./rescuePlan.[pf].layout.js';
-
 
 Page({
     onInit(params) {
         console.log('Rescue Plan page initialized');
     },
-    
+
     build() {
-        hmUI.createWidget(hmUI.widget.TEXT, TEXT_WIDGET);  // Use the imported TEXT_WIDGET
+        // Create text widge
+        hmUI.createWidget(hmUI.widget.TEXT, TEXT_WIDGET);
+
+        // Create the button and add click functionality
+        hmUI.createWidget(hmUI.widget.BUTTON, {
+            // Creates a button widget using the 'BUTTON' type provided by the ZeppOS UI framework.
+            ...TEST_NOTIFICATION_BUTTON,  // Spread operator to include predefined properties from TEST_NOTIFICATION_BUTTON (e.g., positioning, size)
+        
+            click_func: () => {
+                console.log('Testing notification button clicked');
+                
+                // Trigger the notification when the button is clicked
+                notificationMgr.notify({
+                    title: "TEST",  // The title of the notification displayed in the notification center
+                    content: "This is a test notification",  // The body text of the notification
+                    actions: [
+                        {
+                            text: "Ok",  // The text displayed on the notification button (in this case, "Ok")
+                            file: "page/rescuePlan",  // Path to the page that should be opened when the "Ok" button is pressed
+                            // This file path specifies the App Service file (a specific page or view) that will be launched
+                            param: "",  // Optional: Parameters that could be passed along when the page is loaded. In this case, no parameters are provided.
+                        },
+                    ],
+                    vibrate: 6,  // Vibration effect when the notification appears. '6' corresponds to a specific vibration pattern as per the available options:
+                    // 0 - default, 1 - beep, 2 - birdsong, 3 - drumbeat, 4 - gentle, 5 - buzz, 6 - custom vibration pattern
+                });
+            }
+        });
     },
-    
+            
     onDestroy() {
         console.log('Rescue Plan page destroyed');
     },
--- app/page/rescuePlan.r.layout.js
+++ app/page/rescuePlan.r.layout.js
@@ -1,4 +1,8 @@
 import { px } from '@zos/utils';
+import {
+	NORMAL_COLOR,
+	PRESS_COLOR,
+} from '../utils/constants';
 
 export const TEXT_WIDGET = {
     text: 'This is the rescue plan page',
@@ -8,4 +12,16 @@ export const TEXT_WIDGET = {
     h: px(50),  // Adjust height
     text_size: px(30), // Adjust text size
     color: 0xFFFFFF, // White color
+};
+
+export const TEST_NOTIFICATION_BUTTON = {
+    x: px(60),
+    y: px(300),
+    w: px(360),
+    h: px(80),
+    text_size: px(36),
+    radius: px(12),
+    normal_color: NORMAL_COLOR,
+    press_color: PRESS_COLOR,
+    text: "Test Notif",
 };
